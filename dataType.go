14102e222125134aa7cf3dcecdb7ce22d5f3d320
Shanmuga <shanmuga.ss@live.in>
2015-10-14 15:54:07 -0400

    Initial File

---
+++ dataType.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Data Test.\n")
+
+	//With Data type
+	var age int =5
+
+	//Dynamic variable
+	dynAge:=8
+
+		
+
+	//With automatic space
+	fmt.Println(age, dynAge);
+
+	//with custom space
+	fmt.Println(age, "\t", dynAge);
+
+
+	myfloatTest:=48.36
+
+	fmt.Println(myfloatTest);
+
+	stringTest:="Shanmuga";
+	
+	fmt.Println(stringTest);
+}
